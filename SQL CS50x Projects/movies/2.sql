72f4c6103638c2a279290d76b24308d1688bb5d9
Frederick <165361885+Frederick-sudo@users.noreply.github.com>
2025-07-16 17:53:56 +0800

    2.sql

---
+++ SQL CS50x Projects/movies/2.sql
@@ -0,0 +1,5 @@
+2.Write a SQL query to determine the birth year of Emma Stone.
+Your query should output a table with a single column and a single row (not counting the header) containing Emma Stone’s birth year.
+You may assume that there is only one person in the database with the name Emma Stone.
+
+    SELECT name, birth FROM people WHERE name = 'Emma Stone';
